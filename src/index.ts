1f401b9ffffd4b17b5312f1dceef7b914e4df6e1
Redidacove <intellectance022@gmail.com>
2023-01-12 17:00:23 +0530

    css modified

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,62 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import{v4 as uuidV4} from "uuid"
+// console.log(uuidV4());
+type Task ={
+  id:string
+  title: string
+  completed:boolean
+  createdAt:Date
+}
+const list=document.querySelector<HTMLUListElement>("#list");
+const form=document.getElementById("new-task-form") as HTMLFormElement|null
+const input=document.querySelector<HTMLInputElement>("#new-task-title");
+const Tasks:Task[] =loadTasks();
+Tasks.forEach(addListItem)
 
-import confetti from 'canvas-confetti';
+form?.addEventListener("submit", e =>{
+ e.preventDefault();
+ if(input?.value==""||input?.value==null)return;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+ const newTask:Task= {
+  id: uuidV4(),
+  title: input.value,
+  completed:false,
+  createdAt:new Date()
+ }
+ Tasks.push(newTask)
+  addListItem(newTask)
+  saveTasks()
+  input.value=""
+})
+function addListItem(task:Task){
+  const item= document.createElement("li");
+  const label= document.createElement("label");
+  const checkbox= document.createElement("input");
+  checkbox.addEventListener("change", (e) => {
+    task.completed = checkbox.checked;
+    // Find the index of the task in the Tasks array
+    const taskIndex = Tasks.findIndex((t) => t.id === task.id);
+    // Remove the task from the Tasks array
+    Tasks.splice(taskIndex, 1);
+    // Remove the task from local storage
+    localStorage.removeItem("TASKS");
+    // Save the updated Tasks array to local storage
+    saveTasks();
+    // Remove the list item from the DOM
+    item.remove();
+  });
+  
+  checkbox.type="checkbox"
+  checkbox.checked=task.completed
+  label.append(checkbox,task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks(){
+  localStorage.setItem("TASKS",JSON.stringify(Tasks))
+}
+function loadTasks():Task[]{
+ const taskJSON =localStorage.getItem("TASKS")
+ if(taskJSON==null)return []
+  return JSON.parse(taskJSON)
+}
